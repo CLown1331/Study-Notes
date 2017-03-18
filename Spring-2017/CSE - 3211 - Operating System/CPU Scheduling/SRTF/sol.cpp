9d1372253753215b80795fbf6da390957ab1fd6a
Araf Al-Jami <CLown1331@users.noreply.github.com>
2017-03-18 13:15:55 +0600

    First Commit
    TODO: Documentation!

---
+++ Spring-2017/CSE - 3211 - Operating System/CPU Scheduling/SRTF/sol.cpp
@@ -0,0 +1,88 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct process {
+	int arrival_time, burst_time;
+};
+
+struct cmp {
+	bool operator()( const process &a, const process &b ) {
+		return ( a.burst_time > b.burst_time );
+	}
+};
+
+int main() {
+
+	queue < process > Q;
+	priority_queue < process, vector < process >, cmp > PQ;
+
+	process hand, hand2;
+	
+	map < int, int > work;
+
+	int time = 0;
+	
+	int n, no = 0;
+
+	double avg_wt = 0.0, avg_tt = 0.0;
+	
+	cin >> n;
+	
+	vector < process > processList( n );
+	vector < int > AT, BT, WT( n ), TT( n );
+
+	for( process &p: processList ) {
+		cin >> p.arrival_time >> p.burst_time;
+		work[ p.arrival_time ] = ++no;
+		AT.push_back( p.arrival_time );
+		BT.push_back( p.burst_time );
+	}
+
+	sort( processList.begin(), processList.end(), 
+		[]( const process &a, const process &b ) -> bool{ return a.arrival_time < b.arrival_time; } );
+
+	for( process p: processList ) {
+		Q.push( p );
+	}
+
+	while( !Q.empty() || !PQ.empty() ) {
+		if( !Q.empty() ) {
+			hand = Q.front();
+			if( hand.arrival_time <= time ) {
+				Q.pop();
+				PQ.push( hand );
+			}
+			//cout << time << " " << hand.first << " " << hand.second << "\n";
+		}
+		if( !PQ.empty() ) {
+			hand2 = PQ.top();
+			PQ.pop();
+			if( hand2.burst_time > 1 ) {
+				hand2.burst_time -= 1;
+				PQ.push( hand2 );
+			} else {
+				int id = work[ hand2.arrival_time ] - 1;
+				WT[ id ] = time + 1 - hand2.arrival_time - BT[ id ];
+				TT[ id ] = WT[ id ] + BT[ id ];
+				avg_wt += WT[ id ];
+				avg_tt += TT[ id ];
+			}
+			//cout << time << " " << work[ hand2.arrival_time ] << " " << hand2.burst_time << "\n";
+		}
+		time++;
+	}
+
+	printf("-------------------------------------------------------\n");
+
+	printf( "Process\tA.T\tB.T\tW.T\tT.T\tAVG W.T\tAVG T.T\n" );
+
+	for( int i=0; i<n; i++ ) {
+		printf( "P%d\t%2d\t%2d\t%2d\t%2d", i+1, AT[i], BT[i], WT[i], TT[i] );
+		if( i == n / 2 ) printf("\t%.2lf\t%.2lf", avg_wt / n, avg_tt / n );
+		printf( "\n" );
+	}
+
+	printf("-------------------------------------------------------\n");
+
+	return 0;
+}
